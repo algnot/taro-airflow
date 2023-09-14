97dbff01dcf0ffaa3773617b53ed9450d2f0e313
algnot <algnot@Mac-mini-khxng-Thanawat.local>
2023-09-15 00:36:58 +0700

    add Unleash

--- dags/model/config.py
+++ dags/model/config.py
@@ -1,15 +1,34 @@
 from dotenv import load_dotenv
 import os
+from UnleashClient import UnleashClient
 
 
 class Config:
+    client = UnleashClient(
+      url="http://unleash-web:4242/api/",
+      app_name="default",
+      environment="development",
+      project_name="default",
+      custom_headers={'Authorization': os.environ["INIT_CLIENT_API_TOKENS"]})
     
     def __init__(self):
       load_dotenv()
+      client.initialize_client()
       
     def get(self, name, defualt=""):
-        if name in os.environ:
-            return os.environ[name]
+      try:
+        value = client.get_variant("NOTION_BASE_URL") 
         
-        return defualt
+        if "payload" in value and "value" in value["payload"]:
+          return value["payload"]["value"]
+        
+        elif name in os.environ:
+          return os.environ[name]
+        
+        return defualt  
+      except Exception:
+        if name in os.environ:
+          return os.environ[name]
+        
+        return defualt   
     
