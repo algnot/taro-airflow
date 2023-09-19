e13f9830187243236e0ffdfbcd320c82b0d32660
algnot <algnot@Mac-mini-khxng-Thanawat.local>
2023-09-19 18:55:08 +0700

    fix config module

--- dags/model/logger.py
+++ dags/model/logger.py
@@ -2,17 +2,41 @@ from requests import post
 import os
 import datetime
 from dotenv import load_dotenv
+from UnleashClient import UnleashClient
 
 
 class Config:
+    client = UnleashClient(
+      url="http://unleash-web:4242/api/",
+      app_name="default",
+      environment="development",
+      project_name="default",
+      custom_headers={'Authorization': os.environ["INIT_CLIENT_API_TOKENS"]})
+    
     def __init__(self):
       load_dotenv()
+      self.client.initialize_client()
       
     def get(self, name, defualt=""):
-        if name in os.environ:
-            return os.environ[name]
+      try:
+        value = self.client.get_variant(name) 
         
-        return defualt
+        if "payload" in value and "value" in value["payload"]:
+          print(f"Get config {name} from unleash")
+          return value["payload"]["value"]
+        
+        elif name in os.environ:
+          print(f"Get config {name} from env value")
+          return os.environ[name]
+        
+      except Exception as e:
+        print(e)
+        if name in os.environ:
+          print(f"Get config {name} from env value")
+          return os.environ[name]
+        
+        print(f"Get config {name} from defualt value")
+        return defualt   
 
 
 class Logger:
