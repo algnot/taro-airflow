c9584dc1ed27feca0f01e4c3523d9f64ac76196e
algnot <algnot@Mac-mini-khxng-Thanawat.local>
2023-10-20 23:20:16 +0700

    fix up

--- bot/src/index.py
+++ bot/src/index.py
@@ -22,9 +22,13 @@ for file in os.listdir("/src/command"):
     
 async def handle_command_error(interection, error):
     await interection.followup.send("❌ ไม่สามารถใช้คำสั่งได้ โปรดลองใหม่อีกครั้ง", ephemeral=True)
-    logger.error(f"[Discord Command] Can not use command with error\n```{traceback.format_exc()}```\nenv: {config.get('ENV')}")
-        
+    logger.error(f"[Discord Command] Can not use command with error\n```{traceback.format_exc()}```env: `{config.get('ENV')}`")
+
+async def handle_on_bot_error(event, *args, **kwargs):
+    logger.error(f"[Discord Bot] {event} with error\n```{traceback.format_exc()}```env: `{config.get('ENV')}`")
+
 tree.on_error = handle_command_error
+bot.on_error = handle_on_bot_error
             
 @app.route('/', methods=['GET'])
 def index():
@@ -45,7 +49,7 @@ def caller():
                 return str(return_value)
         return "OK"
     except Exception as e:
-        logger.error(f"[Discord Caller] Can not use function {function_id} with error\n```{traceback.format_exc()}```\nenv: {config.get('ENV')}")
+        logger.error(f"[Discord Caller] Can not use function {function_id} with error\n```{traceback.format_exc()}```env: `{config.get('ENV')}`")
         return str(e)
     
 @bot.event
--- dags/common/handle_error.py
+++ dags/common/handle_error.py
@@ -9,7 +9,7 @@ def handle_error(func):
         try:
             return func(*args, **kwargs)
         except Exception as e:
-            logger.error(f"Error when running task: `{func.__name__}` with error\n```{traceback.format_exc()}```\nenv: {config.get('ENV')}")
+            logger.error(f"Error when running task: `{func.__name__}` with error\n```{traceback.format_exc()}```env: `{config.get('ENV')}`")
             raise e
     runner.__name__ = func.__name__
     return runner
